4c14187a32718358cbeb2a048c52702db21b7033
Alex Kyriakidis <hootlex@icloud.com>
2018-10-30 13:17:09 +0100

    the navbar

---
+++ components/TheNavbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <a href="#" class="logo text-lg">
+        Nuxt Fundamentals
+      </a>
+
+      <span class="subheader">A Vue School course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 70px;
+    font-weight: lighter;
+    letter-spacing: 0.5px;
+  }
+  .nav .logo {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  .nav .logo .subheader {
+    opacity: 0.70;
+    font-size: 0.9rem;
+    color: white;
+  }
+  .nav .nav-content {
+    font-size: 1rem;
+  }
+  .nav-content ul {
+    display: flex;
+    list-style: none;
+  }
+  .nav-content ul li {
+    padding: 2px 10px;
+  }
+  .nav-content ul li:not(:first-of-type) {
+    border-left: 1px solid rgb(255, 255, 255, 0.2);
+  }
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
-  <div>
+  <div class="container">
+    <TheNavbar/>
     <nuxt/>
   </div>
 </template>
+
+<script>
+  import TheNavbar from '@/components/TheNavbar'
+  export default {
+    components: {
+      TheNavbar
+    }
+  }
+</script>
 
 <style>
 .button--green {
